b53651efa72ec27391d53ade168a0d20c747d403
lelegoyeng <lelegoyengs@gmail.com>
2023-12-07 12:59:11 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+const storagePath = path.join(__dirname, 'storage');
+
+app.use(express.static(storagePath));
+
+app.get('/listFiles', (req, res) => {
+    fs.readdir(storagePath, (err, files) => {
+        if (err) {
+            return res.status(500).send('Error reading storage directory');
+        }
+
+        res.json({ files });
+    });
+});
+
+app.get('/download/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const filePath = path.join(storagePath, filename);
+
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            return res.status(404).send('File not found');
+        }
+
+        res.download(filePath, filename, (err) => {
+            if (err) {
+                return res.status(500).send('Error downloading file');
+            }
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
